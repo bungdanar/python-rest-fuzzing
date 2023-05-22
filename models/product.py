56cf24193fd37d95b3f847e6d59ca71e200b7f48
Bung Danar <danar.g.putera@gmail.com>
2023-05-22 17:36:26 +0700

    chore: Update product model

--- models/product.py
+++ models/product.py
@@ -1,6 +1,7 @@
+import simplejson as json
+
 from ..common.db import db
 from sqlalchemy.dialects.mysql import INTEGER, DECIMAL, DOUBLE
-from sqlalchemy import DateTime
 from sqlalchemy.sql import func
 
 
@@ -19,7 +20,38 @@ class Product(db.Model):
     weight = db.Column(DOUBLE(unsigned=True), nullable=False)
     note = db.Column(db.String(255), nullable=False)
     published = db.Column(db.Boolean, nullable=False, default=False)
-    created_at = db.Column(DateTime(timezone=True),
-                           nullable=False, server_default=func.now())
-    updated_at = db.Column(DateTime(timezone=True),
-                           nullable=False, onupdate=func.now())
+    created_at = db.Column(
+        db.DateTime, server_default=func.now(), nullable=False)
+    updated_at = db.Column(
+        db.DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    def __init__(self, data):
+        self.name = data.name
+        self.sku = data.sku
+        self.regular_price = data.regular_price
+        self.discount_price = data.discount_price
+        self.quantity = data.quantity
+        self.description = data.description
+        self.weight = data.weight
+        self.note = data.note
+        self.published = data.published
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "sku": self.sku,
+            "regular_price": json.dumps(self.regular_price),
+            "discount_price": json.dumps(self.discount_price),
+            "quantity": self.quantity,
+            "description": self.description,
+            "weight": json.dumps(self.weight),
+            "note": self.note,
+            "published": self.published,
+            "created_at": json.dumps(self.created_at, default=str),
+            "updated_at": json.dumps(self.updated_at, default=str)
+        }
--- resources/product.py
+++ resources/product.py
@@ -1,4 +1,6 @@
-from flask_restful import Resource
+from flask import current_app as app
+from flask_restful import Resource, reqparse
+from ..models.product import Product
 
 
 class ProductResource(Resource):
@@ -8,6 +10,27 @@ class ProductResource(Resource):
         }
 
     def post(self):
-        return {
-            "product": {}
-        }, 201
+        parser = reqparse.RequestParser()
+        parser.add_argument("name")
+        parser.add_argument("sku")
+        parser.add_argument("regular_price")
+        parser.add_argument("discount_price")
+        parser.add_argument("quantity")
+        parser.add_argument("description")
+        parser.add_argument("weight")
+        parser.add_argument("note")
+        parser.add_argument("published")
+
+        data = parser.parse_args()
+        product = Product(data)
+
+        try:
+            product.save_to_db()
+        except Exception as e:
+            app.logger.error(e)
+            return {
+                "message": "Internal Server Error",
+                "statusCode": 500
+            }, 500
+
+        return product.json(), 201
