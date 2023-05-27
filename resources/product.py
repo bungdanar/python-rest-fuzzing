2e1bc14692cebc44776d68ba0e15ab64cfd3744a
Bung Danar <danar.g.putera@gmail.com>
2023-05-27 21:26:55 +0700

    chore: Add product resource with ma validation lv1 and lv2

--- resources/product.py
+++ resources/product.py
@@ -4,7 +4,9 @@ from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
 from common.db import db
-from schemas.product import ProductResponseSchema
+from schemas.product import (ProductResponseSchema,
+                             ProductCreatePayloadWithMaValidationLv1, ProductCreatePayloadWithMaValidationLv2,
+                             ProductQueryWithMaValidationLv1, ProductQueryWithMaValidationLv2)
 from models import ProductModel
 
 blp = Blueprint("product", __name__, description="Operation on product")
@@ -29,6 +31,66 @@ class Product(MethodView):
     def post(self):
         data = request.get_json()
 
+        product = ProductModel(**data)
+
+        try:
+            db.session.add(product)
+            db.session.commit()
+        except SQLAlchemyError as sql_exc:
+            abort(500, message=str(sql_exc))
+        except Exception as exc:
+            abort(500, message=str(exc))
+
+        return product
+
+
+@blp.route("/api/product/ma/lv1")
+class ProductWithMaValidationLv1(MethodView):
+    @blp.arguments(ProductQueryWithMaValidationLv1, location="query")
+    @blp.response(200, ProductResponseSchema(many=True))
+    def get(self, args):
+        try:
+            products = ProductModel.query.filter_by(**args)
+        except SQLAlchemyError as sql_exc:
+            abort(500, message=str(sql_exc))
+        except Exception as exc:
+            abort(500, message=str(exc))
+
+        return products
+
+    @blp.arguments(ProductCreatePayloadWithMaValidationLv1)
+    @blp.response(201, ProductResponseSchema)
+    def post(self, data):
+        product = ProductModel(**data)
+
+        try:
+            db.session.add(product)
+            db.session.commit()
+        except SQLAlchemyError as sql_exc:
+            abort(500, message=str(sql_exc))
+        except Exception as exc:
+            abort(500, message=str(exc))
+
+        return product
+
+
+@blp.route("/api/product/ma/lv2")
+class ProductWithMaValidationLv2(MethodView):
+    @blp.arguments(ProductQueryWithMaValidationLv2, location="query")
+    @blp.response(200, ProductResponseSchema(many=True))
+    def get(self, args):
+        try:
+            products = ProductModel.query.filter_by(**args)
+        except SQLAlchemyError as sql_exc:
+            abort(500, message=str(sql_exc))
+        except Exception as exc:
+            abort(500, message=str(exc))
+
+        return products
+
+    @blp.arguments(ProductCreatePayloadWithMaValidationLv2)
+    @blp.response(201, ProductResponseSchema)
+    def post(self, data):
         product = ProductModel(**data)
 
         try:
--- schemas/product.py
+++ schemas/product.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class ProductResponseSchema(Schema):
@@ -14,3 +14,58 @@ class ProductResponseSchema(Schema):
     published = fields.Boolean()
     created_at = fields.DateTime()
     updated_at = fields.DateTime()
+
+
+class ProductQueryWithMaValidationLv1(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    sku = fields.Str()
+    regular_price = fields.Decimal()
+    discount_price = fields.Decimal()
+    quantity = fields.Int()
+    description = fields.Str()
+    weight = fields.Float()
+    note = fields.Str()
+    published = fields.Boolean()
+    created_at = fields.DateTime()
+
+
+class ProductQueryWithMaValidationLv2(Schema):
+    id = fields.Int()
+    name = fields.Str(validate=validate.Length(min=1, max=255))
+    sku = fields.Str(validate=validate.Length(min=1, max=255))
+    regular_price = fields.Decimal(validate=validate.Range(min=0))
+    discount_price = fields.Decimal(validate=validate.Range(min=0))
+    quantity = fields.Int(validate=validate.Range(min=0))
+    description = fields.Str(validate=validate.Length(min=1, max=1000))
+    weight = fields.Float(validate=validate.Range(min=0))
+    note = fields.Str(validate=validate.Length(min=1, max=255))
+    published = fields.Boolean()
+    created_at = fields.DateTime()
+
+
+class ProductCreatePayloadWithMaValidationLv1(Schema):
+    name = fields.Str(required=True)
+    sku = fields.Str(required=True)
+    regular_price = fields.Decimal(required=True)
+    discount_price = fields.Decimal(required=True)
+    quantity = fields.Int(required=True)
+    description = fields.Str(required=True)
+    weight = fields.Float(required=True)
+    note = fields.Str(required=True)
+    published = fields.Boolean(required=False)
+
+
+class ProductCreatePayloadWithMaValidationLv2(Schema):
+    name = fields.Str(required=True, validate=validate.Length(min=3, max=255))
+    sku = fields.Str(required=True, validate=validate.Length(min=3, max=255))
+    regular_price = fields.Decimal(
+        required=True, places=4, validate=validate.Range(min=0))
+    discount_price = fields.Decimal(
+        required=True, places=4, validate=validate.Range(min=0))
+    quantity = fields.Int(required=True, validate=validate.Range(min=0))
+    description = fields.Str(
+        required=True, validate=validate.Length(min=3, max=1000))
+    weight = fields.Float(required=True, validate=validate.Range(min=0))
+    note = fields.Str(required=True, validate=validate.Length(min=3, max=255))
+    published = fields.Boolean(required=False)
